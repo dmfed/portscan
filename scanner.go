9c2c5238fd91ef3b6a81c6b7a3a60ddbac9e1eb6
Dmitry Fedotov <dafedotov@gmail.com>
2021-06-01 04:02:32 +0300

    moved executable into own package

--- scanner.go
+++ scanner.go
@@ -1,7 +1,6 @@
-package main
+package portscan
 
 import (
-	"flag"
 	"fmt"
 	"net"
 	"sync"
@@ -93,28 +92,4 @@ func scanPorts(addr net.IP, start, end, maxconn int, timeout time.Duration) chan
 		close(comm)
 	}()
 	return comm
-}
-
-func main() {
-	var (
-		address = flag.String("addr", "127.0.0.1", "ip address to connect to")
-		port    = flag.Int("p", 1, "port to connect to")
-		endport = flag.Int("pe", 100, "ending port, if 0, then will only scan -p")
-		maxconn = flag.Int("maxconn", 10, "maximum simultaneous connections to open")
-		timeout = flag.Duration("t", time.Second, "connection timeout")
-	)
-	flag.Parse()
-	_, err := net.ResolveIPAddr("ip", *address)
-	if err != nil {
-		fmt.Println("resolve err:", err)
-		return
-	}
-	scanner := New(*address)
-	if *endport < *port {
-		*endport = *port
-	}
-	scanner.SetPorts(*port, *endport)
-	scanner.SetMaxConn(*maxconn)
-	scanner.SetTimeOut(*timeout)
-	scanner.ScanAndPrint()
 }
